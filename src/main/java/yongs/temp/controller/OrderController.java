5518f32a1d7687eba059d5e81bfbfe6e771ba72e
yongschoi <yongschoi@naver.com>
2022-02-18 19:18:37 +0900

    context-path 방식, version = '1.0.0-SNAPSHOT'

--- src/main/java/yongs/temp/controller/OrderController.java
+++ src/main/java/yongs/temp/controller/OrderController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +19,6 @@ import yongs.temp.vo.User;
 
 @Slf4j
 @RestController
-@RequestMapping("/order")
 public class OrderController {	
 	@Autowired
 	UserClient userClient;
