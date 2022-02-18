6643bc9f718ce74feebb4562e9d3461e862ae855
yongschoi <yongschoi@naver.com>
2022-02-18 20:34:48 +0900

    servlet: context-path 제거

--- src/main/java/yongs/temp/controller/OrderController.java
+++ src/main/java/yongs/temp/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import yongs.temp.vo.User;
 
 @Slf4j
 @RestController
+@RequestMapping("/order")
 public class OrderController {	
 	@Autowired
 	UserClient userClient;
